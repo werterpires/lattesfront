8eaf31d45954c5be26d5ff8497124c05ecced5f9
werterpires <werterpires23@hotmail.com>
2024-03-11 16:28:31 -0300

    create fail data pipe

---
+++ src/app/pipes/fail-data.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FailDataPipe } from './fail-data.pipe';
+
+describe('FailDataPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FailDataPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/fail-data.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'failData',
+  standalone: true,
+})
+export class FailDataPipe implements PipeTransform {
+  transform(value: string | undefined): unknown {
+    if (value === undefined || value.length === 0) {
+      return '###';
+    }
+    return value;
+  }
+}
