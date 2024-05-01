5904b36ea568b3b03fe71ca0a6740657cea7db21
werterpires <werterpires23@hotmail.com>
2024-05-01 13:30:32 -0300

    add new qualitative to professors

--- src/app/professors/professors.component.ts
+++ src/app/professors/professors.component.ts
@@ -1,14 +1,18 @@
-import { Component, Input } from '@angular/core';
-import { QualitativeEventsWorksComponent } from '../sections/trablhos-em-eventos/qualitative-events-works/qualitative-events-works.component';
-import { CurriculumnsService } from '../shared/services/curriculumns.service';
-import { ICurriculum } from '../shared/services/types';
-import { NavigationEnd, Router } from '@angular/router';
-import { AccordionComponent } from '../shared/accordion/accordion.component';
-import { NgClass, NgFor, NgIf } from '@angular/common';
-import { ContainerComponent } from '../shared/container/container.component';
-import { filter } from 'rxjs/operators';
-import { QuantitativeProfessorsComponent } from './quantitative-professors/quantitative-professors.component';
-import { ProfessorDataComponent } from './professor-data/professor-data.component';
+import { Component, Input } from '@angular/core'
+import { CurriculumnsService } from '../shared/services/curriculumns.service'
+import { ICurriculum } from '../shared/services/types'
+import { NavigationEnd, Router } from '@angular/router'
+import { AccordionComponent } from '../shared/accordion/accordion.component'
+import { NgClass, NgFor, NgIf } from '@angular/common'
+import { ContainerComponent } from '../shared/container/container.component'
+import { filter } from 'rxjs/operators'
+import { QuantitativeProfessorsComponent } from './quantitative-professors/quantitative-professors.component'
+import { ProfessorDataComponent } from './professor-data/professor-data.component'
+import { QualitativeSectionComponent } from '../shared/qualitative-section/qualitative-section.component'
+import { Participacao, TrabalhoEmEventos } from '../shared/services/objTypes'
+import * as eventProps from '../sections/trablhos-em-eventos/props'
+import * as participationsProps from '../sections/outras-participacoes-em-Eventos-Congressos/props'
+import { Props } from '../shared/quantitative-section/tpes'
 
 @Component({
   selector: 'app-professors',
@@ -19,58 +23,108 @@ import { ProfessorDataComponent } from './professor-data/professor-data.componen
     NgClass,
     ContainerComponent,
     AccordionComponent,
-    QualitativeEventsWorksComponent,
     QuantitativeProfessorsComponent,
     ProfessorDataComponent,
+    QualitativeSectionComponent
   ],
   templateUrl: './professors.component.html',
-  styleUrl: './professors.component.css',
+  styleUrl: './professors.component.css'
 })
 export class ProfessorsComponent {
-  curriculums: ICurriculum[] = [];
-  professorCurriculum: ICurriculum[] = [];
-  professorsToShow: ICurriculum[] = [];
-  filterString: string = '';
-  onlyActives: boolean = false;
-  @Input() professorsPage: boolean = true;
-  @Input() head: boolean = true;
+  curriculums: ICurriculum[] = []
+  professorCurriculum: ICurriculum[] = []
+  professorsToShow: ICurriculum[] = []
+  filterString: string = ''
+  onlyActives: boolean = false
+  @Input() professorsPage: boolean = true
+  @Input() head: boolean = true
+
+  allProfessorSections: {
+    trabalhosEmEventos: TrabalhoEmEventos[]
+    outrasParticipacoesEmEventosCongressos: Participacao[]
+  } = {
+    trabalhosEmEventos: [],
+    outrasParticipacoesEmEventosCongressos: []
+  }
+
+  eventsProps: Props[] = eventProps.eventProps
+  workProps: Props[] = eventProps.workProps
+  participationsQuantiProps: Props[] = participationsProps.participationProps
+  participationsQualiProps: Props[] =
+    participationsProps.participationQualyProps
+
   constructor(
-    private curriculumnsService: CurriculumnsService,
-    private router: Router
+    private readonly curriculumnsService: CurriculumnsService,
+    private readonly router: Router
   ) {
     this.curriculumnsService.curriculumns$.subscribe((curriculumns) => {
-      this.curriculums = curriculumns;
-      this.filterCurriculums();
-      this.getProfessorCurriculum();
-    });
+      this.curriculums = curriculumns
+      this.filterCurriculums()
+      this.getProfessorCurriculum()
+    })
 
     this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe(() => {
-        this.getProfessorCurriculum();
-      });
+        this.getProfessorCurriculum()
+      })
   }
 
-  getProfessorCurriculum() {
-    const url = window.location.href;
-    const parts = url.split('/');
-    const professorId = parts.pop();
+  getProfessorCurriculum(): void {
+    const url = window.location.href
+    const parts = url.split('/')
+    const professorId = parts.pop()
 
-    if (professorId) {
-      this.professorCurriculum = this.curriculums.filter(
-        (curriculum) => curriculum.lattesId === professorId
-      );
+    if (!professorId) {
+      return
     }
+
+    this.professorCurriculum = this.curriculums.filter(
+      (curriculum) => curriculum.lattesId === professorId
+    )
+
+    if (this.professorCurriculum.length === 0) {
+      return
+    }
+
+    this.allProfessorSections.trabalhosEmEventos =
+      this.professorCurriculum[0].curriculum.trabalhosEmEventos
+    this.allProfessorSections.trabalhosEmEventos =
+      this.allProfessorSections.trabalhosEmEventos.map((work) => ({
+        ano: work.anoDeRealizacao,
+        nome: this.professorCurriculum[0].curriculum.nome,
+        lattesid: this.professorCurriculum[0].lattesId,
+        active: this.professorCurriculum[0].active,
+        serviceYears: this.professorCurriculum[0].serviceYears,
+        ...work
+      }))
+
+    this.allProfessorSections.outrasParticipacoesEmEventosCongressos =
+      this.professorCurriculum[0].curriculum.outrasParticipacoesEmEventosCongressos
+
+    this.allProfessorSections.outrasParticipacoesEmEventosCongressos =
+      this.allProfessorSections.outrasParticipacoesEmEventosCongressos.map(
+        (participation) => ({
+          ano: participation.ano,
+          nome: this.professorCurriculum[0].curriculum.nome,
+          lattesid: this.professorCurriculum[0].lattesId,
+          active: this.professorCurriculum[0].active,
+          serviceYears: this.professorCurriculum[0].serviceYears,
+          ...participation
+        })
+      )
   }
 
-  navigateTo(url: string) {
-    this.router.navigate(['professors/' + url]);
+  async navigateTo(url: string): Promise<void> {
+    await this.router.navigate(['professors/' + url])
   }
-  filterCurriculums() {
+
+  filterCurriculums(): void {
     const filters = this.filterString
       .toLowerCase()
       .split(' ')
-      .map((filter) => filter.trim());
+      .map((filter) => filter.trim())
+
     this.professorsToShow = this.curriculums.filter((curriculum) => {
       return (
         (filters.some((filter) =>
@@ -83,7 +137,7 @@ export class ProfessorsComponent {
             curriculum.serviceYears.toLowerCase().includes(filter)
           )) &&
         (this.onlyActives ? curriculum.active : true)
-      );
-    });
+      )
+    })
   }
 }
---
+++ src/app/sections/trablhos-em-eventos/props.ts
@@ -0,0 +1,334 @@
+import { Props } from 'src/app/shared/quantitative-section/tpes'
+
+export const workProps: Props[] = [
+  {
+    name: 'Professor',
+    key: 'nome',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '320px'
+  },
+
+  {
+    name: 'Ano',
+    key: 'anoDeRealizacao',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '40px'
+  }
+]
+
+export const eventProps: Props[] = [
+  {
+    name: 'Professor',
+    key: 'nome',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '240px'
+  },
+  {
+    name: 'Título do trabalho',
+    key: 'tituloDoTrabalho',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '320px'
+  },
+  {
+    name: 'Natureza',
+    key: 'natureza',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '120px'
+  },
+  {
+    name: 'Ano do trabalho',
+    key: 'anoDoTrabalho',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '120px'
+  },
+  {
+    name: 'País do evento',
+    key: 'paisDoEvento',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '120px'
+  },
+
+  {
+    name: 'Idioma',
+    key: 'idioma',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '80px'
+  },
+  {
+    name: 'Meio de divulgacão',
+    key: 'meioDeDivulgacao',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '120px'
+  },
+  {
+    name: 'DOI',
+    key: 'doi',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '120px'
+  },
+  {
+    name: 'URL',
+    key: 'homePageDoTrabalho',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '240px'
+  },
+
+  {
+    name: 'Relevância?',
+    key: 'flagRelevancia',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '80px'
+  },
+
+  {
+    name: 'Título do trabalho em inglês',
+    key: 'tituloDoTrabalhoIngles',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '240px'
+  },
+
+  {
+    name: 'Divulgação científica?',
+    key: 'flagDivulgacaoCientifica',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '80px'
+  },
+
+  {
+    name: 'Nome do evento',
+    key: 'nomeDoEvento',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '240px'
+  },
+
+  {
+    name: 'Classificação do evento',
+    key: 'classificacaoDoEvento',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '120px'
+  },
+
+  {
+    name: 'Cidade do evento',
+    key: 'cidadeDoEvento',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '120px'
+  },
+
+  {
+    name: 'Ano de realização',
+    key: 'anoDeRealizacao',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '120px'
+  },
+
+  {
+    name: 'Título dos anais ou proceedings',
+    key: 'tituloDosAnaisOuProceedings',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '240px'
+  },
+
+  {
+    name: 'Volume',
+    key: 'volume',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '80px'
+  },
+
+  {
+    name: 'Fasciculo',
+    key: 'fasciculo',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '80px'
+  },
+
+  {
+    name: 'Serie',
+    key: 'serie',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '80px'
+  },
+
+  {
+    name: 'Pagina inicial',
+    key: 'paginaInicial',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '80px'
+  },
+
+  {
+    name: 'Pagina final',
+    key: 'paginaFinal',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '80px'
+  },
+
+  {
+    name: 'ISBN',
+    key: 'isbn',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '80px'
+  },
+
+  {
+    name: 'Nome da editora',
+    key: 'nomeDaEditora',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '160px'
+  },
+
+  {
+    name: 'Cidade da editora',
+    key: 'cidadeDaEditora',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '120px'
+  },
+
+  {
+    name: 'Nome do evento em inglês',
+    key: 'nomeDoEventoIngles',
+    showFilter: false,
+    ascending: true,
+    filterObject: {
+      text: [],
+      disjunctive: true
+    },
+    width: '240px'
+  }
+]
